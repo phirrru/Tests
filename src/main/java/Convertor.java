b112412943b3a1f549157f79f6d26986db6b79a3
phiru <phiru@inbox.ru>
2022-10-22 21:04:34 +0300

    Added code for starting units methods

--- src/main/java/Convertor.java
+++ src/main/java/Convertor.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Convertor {
+    public static String startingUnits(int n) {
+        if (n == 1)
+            return "Длина";
+        else if (n == 2)
+            return "Масса";
+        else return "Отсутствует в списке";
+    }
+
+    public static String LengthUnit(int n) {
+        if (n == 1)
+            return "Верста";
+        else if (n == 2)
+            return "Сажень";
+        else if (n == 3)
+            return "Аршин";
+        else if (n == 4)
+            return "Локоть";
+        else if (n == 5)
+            return "Фут";
+        else if (n == 6)
+            return "Вершок";
+        else return "Отсутствует в списке";
+    }
 }
